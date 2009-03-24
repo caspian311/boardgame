c5b4fc538e7f04f370cf4cbd4a674365116f41c5
matt <matt@d5caad04-189b-467d-8c69-358c65116b6f>
2009-03-24 23:59:40 +0000

    fixed annoying little animation at the beginning
    git-svn-id: http://svn.todd.net/svn/games/boardgame@311 d5caad04-189b-467d-8c69-358c65116b6f

--- net.todd.games.boardgame/src/main/java/net/todd/games/boardgame/PieceGroup.java
+++ net.todd.games.boardgame/src/main/java/net/todd/games/boardgame/PieceGroup.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.media.j3d.Alpha;
 import javax.media.j3d.Bounds;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Group;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
@@ -15,14 +16,18 @@ import com.sun.j3d.utils.behaviors.interpolators.KBKeyFrame;
 import com.sun.j3d.utils.behaviors.interpolators.KBRotPosScaleSplinePathInterpolator;
 
 public class PieceGroup extends BranchGroup implements IPieceGroup {
-	private KBRotPosScaleSplinePathInterpolator linearInterpolator;
 	private KBKeyFrame[] linearKeyFrames;
 	private final Color3f color;
+	private final TransformGroup userPieceTransformGroup;
+	private final Bounds bounds;
+	private Vector3f previousPosition;
 
 	public PieceGroup(Bounds bounds, Vector3f startingPoint, Color3f color) {
+		this.bounds = bounds;
 		this.color = color;
-		TransformGroup userPieceTransformGroup = new TransformGroup(new Transform3D());
+		userPieceTransformGroup = new TransformGroup(new Transform3D());
 		userPieceTransformGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		userPieceTransformGroup.setCapability(Group.ALLOW_CHILDREN_EXTEND);
 		SelectablePiece userPiece = new SelectablePiece(this);
 
 		Transform3D piecePosition = new Transform3D();
@@ -31,13 +36,16 @@ public class PieceGroup extends BranchGroup implements IPieceGroup {
 		pieceTG.addChild(userPiece);
 
 		userPieceTransformGroup.addChild(pieceTG);
-		createMovingBehavior(userPieceTransformGroup, bounds);
 
 		addChild(userPieceTransformGroup);
 		movePieceTo(startingPoint);
 	}
 
-	private void createMovingBehavior(TransformGroup userPieceTransformGroup, Bounds bounds) {
+	public Color3f getColor() {
+		return color;
+	}
+
+	public void movePieceTo(final Vector3f position) {
 		BranchGroup behaviorBranch = new BranchGroup();
 		linearKeyFrames = new KBKeyFrame[2];
 		linearKeyFrames[0] = createKeyFrameAtPosition(new Vector3f(new float[] { 0f, 0f, 0f }));
@@ -48,32 +56,34 @@ public class PieceGroup extends BranchGroup implements IPieceGroup {
 		Transform3D yAxis = new Transform3D();
 		Alpha animAlpha = new Alpha(1, 2000);
 
-		linearInterpolator = new KBRotPosScaleSplinePathInterpolator(animAlpha,
-				userPieceTransformGroup, yAxis, linearKeyFrames);
+		KBRotPosScaleSplinePathInterpolator linearInterpolator = new KBRotPosScaleSplinePathInterpolator(
+				animAlpha, userPieceTransformGroup, yAxis, linearKeyFrames);
 		linearInterpolator.setSchedulingBounds(bounds);
 		linearInterpolator.setEnable(false);
 
 		behaviorBranch.addChild(linearInterpolator);
 		userPieceTransformGroup.addChild(behaviorBranch);
-	}
 
-	public void movePieceTo(final Vector3f position) {
 		linearInterpolator.setEnable(false);
-		linearKeyFrames[0] = linearKeyFrames[1];
+		if (previousPosition == null) {
+			previousPosition = position;
+		}
+		linearKeyFrames[0] = createKeyFrameAtPosition(previousPosition);
 		linearKeyFrames[0].knot = 0;
 		linearKeyFrames[1] = createKeyFrameAtPosition(position);
 		linearKeyFrames[1].knot = 1;
 		linearInterpolator.setKeyFrames(linearKeyFrames);
 		linearInterpolator.getAlpha().setStartTime(new Date().getTime());
 		linearInterpolator.setEnable(true);
+
+		previousPosition = position;
 	}
 
 	private KBKeyFrame createKeyFrameAtPosition(Vector3f position) {
+		if (position == null) {
+			position = new Vector3f(0, 0, 0);
+		}
 		return new KBKeyFrame(1f, 0, new Point3f(position), 0f, 0f, 0, new Point3f(1, 1, 1), 0f,
 				0f, 0f);
-	}
-
-	public Color3f getColor() {
-		return color;
 	}
 }
