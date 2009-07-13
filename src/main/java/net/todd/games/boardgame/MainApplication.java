f4a667a2ecde4c7cda85ab8fd61bc4ef15d4fd81
matt.todd <matt.c.todd@gmail.com>
2009-07-13 09:26:16 -0600

    hitting the escape key switches from full screen to centered window

--- src/main/java/net/todd/games/boardgame/MainApplication.java
+++ src/main/java/net/todd/games/boardgame/MainApplication.java
@@ -1,22 +1,42 @@
 package net.todd.games.boardgame;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.media.j3d.Canvas3D;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
 public class MainApplication {
-	private final IUniverse universe;
-	private final JFrame frame;
 	private final IGameLauncher gameLauncher;
+	private final String title;
+
+	private final Canvas3D canvas3D;
+	private final IUniverse universe;
+
+	private JFrame frame;
 
 	public MainApplication(String title, IGameLauncher gameLauncher) {
+		this.title = title;
 		this.gameLauncher = gameLauncher;
+
+		canvas3D = createCanvas();
+		universe = new UniverseFactory(canvas3D).createUniverse();
+
+		createFullScreenFrame();
+	}
+
+	private void createFullScreenFrame() {
+		closePreviousFrame();
+
 		frame = new JFrame(title);
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
@@ -24,25 +44,78 @@ public class MainApplication {
 				System.exit(0);
 			}
 		});
+
+		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		frame.setUndecorated(true);
+
+		canvas3D.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == 27) {
+					createWindowFrame();
+				}
+			}
+
+		});
+		frame.getContentPane().add(canvas3D, BorderLayout.CENTER);
+
+		frame.setVisible(true);
+	}
+
+	private void createWindowFrame() {
+		closePreviousFrame();
+
+		frame = new JFrame(title);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+
 		frame.setSize(1000, 800);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int topLeftXCoordinate = (screenSize.width / 2) - (1000 / 2);
+		int topLeftYCoordinate = (screenSize.height / 2) - (800 / 2);
+		frame.setLocation(topLeftXCoordinate, topLeftYCoordinate);
 
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new BorderLayout());
+		canvas3D.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == 27) {
+					createFullScreenFrame();
+				}
+			}
 
+		});
+		frame.getContentPane().add(canvas3D, BorderLayout.CENTER);
+
+		frame.setVisible(true);
+	}
+
+	private void closePreviousFrame() {
+		if (frame != null) {
+			for (WindowListener windowListener : frame.getWindowListeners()) {
+				frame.removeWindowListener(windowListener);
+			}
+			for (KeyListener keyListener : canvas3D.getKeyListeners()) {
+				canvas3D.removeKeyListener(keyListener);
+			}
+			frame.setVisible(false);
+			frame.dispose();
+		}
+	}
+
+	private Canvas3D createCanvas() {
 		Canvas3D canvas3D = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
 		canvas3D.setFocusable(true);
 		canvas3D.requestFocus();
-		mainPanel.add(canvas3D, BorderLayout.CENTER);
-
-		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
-
-		universe = new UniverseFactory(canvas3D).createUniverse();
+		return canvas3D;
 	}
 
 	public void start() {
 		IBranchGroupFactory branchGroupFactory = new BranchGroupFactory();
 		IGameEngineFactory gameEngineFactory = new GameEngineFactory();
 		gameLauncher.launchGame(universe, branchGroupFactory, gameEngineFactory);
-		frame.setVisible(true);
 	}
 }
